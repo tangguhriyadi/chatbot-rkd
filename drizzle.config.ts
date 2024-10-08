cfd48e75f73e61c25362a570df1a95049e3ee75e
tangguhriyadi <mtangguh97@gmail.com>
2024-10-08 16:08:29 +0700

    add drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is required");
+
+export default defineConfig({
+    schema: "./src/db/schema.ts",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL,
+    },
+});
